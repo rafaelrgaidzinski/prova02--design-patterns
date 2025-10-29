fc7418af645e283bed9a6392fc33924aa9b1c2a9
Rafael Ronsoni Gaidzinski <rafael.513011@alunosatc.edu.br>
2025-10-29 20:17:58 -0300

    Resolvido os design patterns adapter e decorator

--- desafio02/index01.js
+++ desafio02/index01.js
@@ -1,1 +1,42 @@
-console.log("Hello, World! 001");
+class LegacyPaymentSystem {
+    makePayment(amount) {
+        console.log(`Pagando R$${amount} com sistema legado.`);
+    }
+}
+  
+class ModernPaymentAPI {
+    process(amountInCents) {
+        console.log(`Pagamento de R$${amountInCents / 100} via API moderna.`);
+    }
+}
+  
+class ModernPaymentAdapter extends LegacyPaymentSystem {
+    constructor(modernPayment) {
+        super();
+        this.modernPayment = modernPayment;
+    }
+
+    makePayment(amount) {
+        this.modernPayment.process(amount);
+    }
+}
+  
+class PaymentProcessor {
+    constructor(system) {
+        this.system = system;
+    }
+
+    pay(amount) {
+        this.system.makePayment(amount);
+    }
+}
+  
+// Cliente
+const legacy = new LegacyPaymentSystem();
+const processor = new PaymentProcessor(legacy);
+processor.pay(100);
+
+const modernPayment = new ModernPaymentAPI();
+const modernPaymentAdapter = new ModernPaymentAdapter(modernPayment);
+const modernProcessor = new PaymentProcessor(modernPaymentAdapter);
+modernProcessor.pay(200);
--- desafio02/index02.js
+++ desafio02/index02.js
@@ -1,1 +1,46 @@
-console.log("Hello, World! 002");
+class Message {
+    constructor(text) {
+        this.text = text;
+    }
+
+    getText() {
+        return this.text;
+    }
+}
+
+// Decorator base
+class MessageDecorator {
+    constructor(message) {
+        this.message = message;
+    }
+
+    getText() {
+        return this.message.getText();
+    }
+}
+  
+// Decorator concreto: envia e-mail
+class EmailMessage extends MessageDecorator {
+    getText() {
+        const text = super.getText();
+        console.log("Enviando mensagem por e-mail:", text);
+        return text;
+    }
+}
+  
+// Decorator concreto: envia SMS
+class SMSMessage extends MessageDecorator {
+    getText() {
+        const text = super.getText();
+        console.log("Enviando mensagem por SMS:", text);
+        return text;
+    }
+}
+
+// Cliente
+const msg = new Message("hoje o dia está horrível");
+console.log(msg.getText());
+  
+// Cliente: empilha os decoradores
+const message = new SMSMessage(new EmailMessage(new Message("Hoje o dia está bonito")));
+message.getText();
